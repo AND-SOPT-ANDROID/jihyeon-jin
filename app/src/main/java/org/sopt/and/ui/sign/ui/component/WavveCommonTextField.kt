55f53b1460b6afab79b471857a059377cf9e9b44
serioushyeon <zz64446949@gmail.com>
2024-10-11 02:06:38 +0900

    [UI] 일반 텍스트 필드 구현

---
+++ app/src/main/java/org/sopt/and/ui/sign/ui/component/WavveCommonTextField.kt
@@ -0,0 +1,69 @@
+package org.sopt.and.ui.sign.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.ui.theme.Gray5
+import org.sopt.and.ui.theme.WavveDisabled
+
+@Composable
+fun WavveCommonTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        textStyle = TextStyle(color = Color.White, fontSize = 16.sp),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ) {
+                if (value.isEmpty()) {
+                    Text(text = hint, color = WavveDisabled, fontSize = 16.sp)
+                }
+                innerTextField()
+
+            }
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+                keyboardController?.hide()
+            }
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Gray5,
+                shape = RoundedCornerShape(7.dp),
+            )
+            .height(48.dp)
+    )
+}
