69166e55934415a805c4a60b20fe9bbd6b3562e8
serioushyeon <zz64446949@gmail.com>
2024-10-11 03:25:07 +0900

    ui/#3 : 마이페이지 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/and/ui/mypage/component/MyPageContents.kt
@@ -0,0 +1,60 @@
+package org.sopt.and.ui.mypage.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
+import org.sopt.and.ui.theme.WavveDisabled
+
+@Composable
+fun MyPageContents(
+    title: String,
+    information: String,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(
+                    R.drawable.ic_no_content
+                ),
+                contentDescription = "No Content",
+                modifier = Modifier.size(54.dp)
+            )
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Text(
+                text = information,
+                color = WavveDisabled
+            )
+        }
+    }
+}
