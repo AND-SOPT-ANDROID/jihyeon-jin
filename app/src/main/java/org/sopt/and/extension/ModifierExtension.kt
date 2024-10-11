230a1153edd771d39a64bf44e379e55f448b6757
serioushyeon <zz64446949@gmail.com>
2024-10-11 12:59:11 +0900

    add/#1 : extension추가

---
+++ app/src/main/java/org/sopt/and/extension/ModifierExtension.kt
@@ -0,0 +1,34 @@
+package org.sopt.and.extension
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+@OptIn(ExperimentalFoundationApi::class)
+inline fun Modifier.noRippleClickable(
+    crossinline onClick: () -> Unit = {},
+    crossinline onLongClick: () -> Unit = {}
+): Modifier = composed {
+    this.clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+    }
+    this.combinedClickable(
+        onClick = { onClick() },
+        onLongClick = { onLongClick() },
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+inline fun Modifier.noRippleClickable(
+    crossinline onClick: () -> Unit = {},
+): Modifier = composed {
+    this.clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
+
+}
