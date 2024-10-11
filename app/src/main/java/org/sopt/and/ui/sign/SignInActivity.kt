24e936d0bf49baf6258e43bf57cdb94ebbda7378
serioushyeon <zz64446949@gmail.com>
2024-10-11 16:06:57 +0900

    chore/#3 : SignInActivity 가독성 높이기

--- app/src/main/java/org/sopt/and/ui/sign/SignInActivity.kt
+++ app/src/main/java/org/sopt/and/ui/sign/SignInActivity.kt
@@ -1,5 +1,6 @@
 package org.sopt.and.ui.sign
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,6 +21,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.sopt.and.R
 import org.sopt.and.ui.component.topBar.BackButtonTopBar
@@ -36,25 +39,20 @@ class SignInActivity : ComponentActivity() {
         setContent {
             ANDANDROIDTheme {
                 val context = LocalContext.current
-                var textFieldEmail by remember {
-                    mutableStateOf("")
-                }
-                 var textFieldPassword by remember {
-                     mutableStateOf("")
-                 }
-                val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
-                    if (result.resultCode == RESULT_OK) {
-                        val data = result.data
-                        email = data?.getStringExtra("EMAIL")?:""
-                        password = data?.getStringExtra("PASSWORD")?:""
-                    }
-                }
+                var textFieldEmail by remember { mutableStateOf("") }
+                var textFieldPassword by remember { mutableStateOf("") }
+
+                val launcher = setupActivityResultLauncher()
+
                 val snackbarHostState = remember { SnackbarHostState() }
                 val scope = rememberCoroutineScope()
-                Scaffold(topBar = {
-                    BackButtonTopBar({  })
-                }, snackbarHost = { SnackbarHost(snackbarHostState) } ,
-                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                // 화면 구성
+                Scaffold(
+                    topBar = { BackButtonTopBar({ /*뒤로가기처리*/}) },
+                    snackbarHost = { SnackbarHost(snackbarHostState) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     SignInScreen(
                         modifier = Modifier.padding(innerPadding),
                         email = textFieldEmail,
@@ -65,39 +63,74 @@ class SignInActivity : ComponentActivity() {
                             launcher.launch(Intent(this, SignUpActivity::class.java))
                         },
                         onSignInButtonClick = {
-                            if (email == textFieldEmail && password == textFieldPassword) {
-                                if (email.isNotBlank() && password.isNotBlank()) {
-                                    PreferenceUtils.saveUserId(context, email)
-                                    PreferenceUtils.saveUserPassword(context, password)
-                                    val intent = Intent(context, MyActivity::class.java).apply {
-                                        putExtra("SNACKBAR_MESSAGE",
-                                            getString(R.string.sign_in_snackbar_login_success))
-                                    }
-                                    startActivity(intent)
-                                    finish()
-                                }
-                                else{
-                                    scope.launch {
-                                        snackbarHostState.showSnackbar(
-                                            message = getString(R.string.sign_in_snackbar_enter_id_password),
-                                            actionLabel = getString(R.string.sign_in_snackbar_action_close)
-                                        )
-                                    }
-                                }
-                            } else {
-                                scope.launch {
-                                    snackbarHostState.showSnackbar(
-                                        message = getString(R.string.sign_in_snackbar_error_id_pw),
-                                        actionLabel = getString(R.string.sign_in_snackbar_action_close)
-                                    )
-                                }
-                            }
+                            handleSignIn(
+                                context = context,
+                                snackbarHostState = snackbarHostState,
+                                scope = scope,
+                                emailInput = textFieldEmail,
+                                passwordInput = textFieldPassword
+                            )
                         }
                     )
                 }
             }
         }
     }
+
+    @Composable
+    private fun setupActivityResultLauncher() =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+                email = data?.getStringExtra("EMAIL") ?: ""
+                password = data?.getStringExtra("PASSWORD") ?: ""
+            }
+        }
+
+    // 로그인 처리 함수
+    private fun handleSignIn(
+        context: Context,
+        snackbarHostState: SnackbarHostState,
+        scope: CoroutineScope,
+        emailInput: String,
+        passwordInput: String
+    ) {
+        if (email == emailInput && password == passwordInput) {
+            if (emailInput.isNotBlank() && passwordInput.isNotBlank()) {
+                PreferenceUtils.saveUserId(context, emailInput)
+                PreferenceUtils.saveUserPassword(context, passwordInput)
+
+                val intent = Intent(context, MyActivity::class.java).apply {
+                    putExtra("SNACKBAR_MESSAGE", context.getString(R.string.sign_in_snackbar_login_success))
+                }
+                startActivity(intent)
+                finish()
+            } else {
+                showSnackbar(
+                    snackbarHostState = snackbarHostState,
+                    scope = scope,
+                    message = context.getString(R.string.sign_in_snackbar_enter_id_password)
+                )
+            }
+        } else {
+            showSnackbar(
+                snackbarHostState = snackbarHostState,
+                scope = scope,
+                message = context.getString(R.string.sign_in_snackbar_error_id_pw)
+            )
+        }
+    }
+
+    private fun showSnackbar(
+        snackbarHostState: SnackbarHostState,
+        scope: CoroutineScope,
+        message: String
+    ) {
+        scope.launch {
+            snackbarHostState.showSnackbar(
+                message = message,
+                actionLabel = getString(R.string.sign_in_snackbar_action_close)
+            )
+        }
+    }
 }
-
-
