28045b465b8224247a8a597d97d49b6acff03312
serioushyeon <zz64446949@gmail.com>
2024-10-11 13:44:45 +0900

    feat/#1 : 자동로그인

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -4,13 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import org.sopt.and.ui.mypage.MyActivity
 import org.sopt.and.ui.sign.SignInActivity
+import org.sopt.and.utils.PreferenceUtils
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val intent = Intent(this, SignInActivity::class.java)
+        
+        val id = PreferenceUtils.getUserId(this) ?:""
+        val pw = PreferenceUtils.getUserPassword(this)?:""
+
+        val intent = if (id.isNotBlank() && pw.isNotBlank()) {
+            Intent(this, MyActivity::class.java)
+        } else {
+            Intent(this, SignInActivity::class.java)
+        }
+
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
+        finish()
     }
 }
