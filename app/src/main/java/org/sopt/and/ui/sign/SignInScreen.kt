4e26209ddc3f1a4302a9c566d62c5a89f0f6086f
serioushyeon <zz64446949@gmail.com>
2024-10-11 12:58:56 +0900

    mod/#3 : 리플제거

--- app/src/main/java/org/sopt/and/ui/sign/SignInScreen.kt
+++ app/src/main/java/org/sopt/and/ui/sign/SignInScreen.kt
@@ -3,6 +3,7 @@ package org.sopt.and.ui.sign
 import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,11 +18,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.sopt.and.extension.noRippleClickable
 import org.sopt.and.ui.component.ServiceAccountItemRow
 import org.sopt.and.ui.component.textField.WavveCommonPasswordField
 import org.sopt.and.ui.component.textField.WavveCommonTextField
@@ -64,13 +67,14 @@ fun SignInScreen(modifier: Modifier = Modifier,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 8.dp)
+                .noRippleClickable()
             ,
             colors = ButtonDefaults.buttonColors(
                 containerColor = WavvePrimary
             ),
             onClick = {
                 onSignInButtonClick()
-            }
+            },
         ) {
             Text(
                 text = "로그인",
@@ -116,7 +120,8 @@ fun SignInScreen(modifier: Modifier = Modifier,
                 fontSize = 12.sp,
                 color = Gray3,
                 modifier = Modifier
-                    .clickable {
+                    .clickable(interactionSource = remember { MutableInteractionSource() },
+                        indication = null) {
                         onSignUpButtonClick()
                     }
             )
