72c732c0dea05dfec1c56702bfc3119a532e04ab
serioushyeon <zz64446949@gmail.com>
2024-11-15 20:38:17 +0900

    chore/#7 : 주석 추가

--- app/src/main/java/org/sopt/and/data/common/ErrorTypeWithMessage.kt
+++ app/src/main/java/org/sopt/and/data/common/ErrorTypeWithMessage.kt
@@ -1,6 +1,7 @@
 package org.sopt.and.data.common
 
 //에러에 따른 액션과 메시지를 위한 객체
+//TODO: 추후 enum으로 교체 가능
 object ErrorTypeWithMessage {
     const val INVALID_TOKEN = "다시 로그인 해주세요." //토큰이 유효하지 않거나 없음, 다시 로그인 하도록 유도함
 }
