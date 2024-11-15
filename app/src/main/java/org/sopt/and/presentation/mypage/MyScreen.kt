7f0bdecfd00e474f4e8609f3073b50fe5cae58b1
serioushyeon <zz64446949@gmail.com>
2024-11-15 20:40:02 +0900

    del/#7 : 불필요 코드 삭제

--- app/src/main/java/org/sopt/and/presentation/mypage/MyScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/mypage/MyScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
