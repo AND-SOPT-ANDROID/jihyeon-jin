e40be8945570344b037b7779b184ea7fef5d0298
serioushyeon <zz64446949@gmail.com>
2024-11-15 16:45:25 +0900

    add/#7 : 라이브러리 추가 (수정)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 val properties = Properties().apply {
@@ -37,11 +38,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17 //윈도우에서 hilt 사용 시 java 1.8은 에러가 발생하는 이슈가 있다고함 -> 17 사용 시 해결
+        targetCompatibility = JavaVersion.VERSION_17 //윈도우에서 hilt 사용 시 java 1.8은 에러가 발생하는 이슈가 있다고함 -> 17 사용 시 해결
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17" //윈도우에서 hilt 사용 시 java 1.8은 에러가 발생하는 이슈가 있다고함 -> 17 사용 시 해결
     }
     buildFeatures {
         compose = true
@@ -81,16 +82,9 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     // hilt
-    implementation(libs.hilt)
-    implementation(libs.hilt.compiler)
-
-    // hilt testing
-    implementation(libs.hilt.android.testing)
-
-    testImplementation(libs.hilt.android.testing)
-    testAnnotationProcessor(libs.hilt.android.compiler)
-
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
     androidTestImplementation(libs.hilt.android.testing)
-
-
+    kspAndroidTest(libs.hilt.compiler)
 }
